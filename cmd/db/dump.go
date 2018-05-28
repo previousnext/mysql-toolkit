e95b629576263a3a787014242f0422432bda5355
Nick Santamaria <nick@nicksantamaria.net>
2018-05-28 16:28:34 +1000

    Enables sending dump data to stdout. (#8)
    * Refactored Dump() to accept io.Writer for the sql destination.
    * Added ability to output dump to stdout.

--- cmd/db/dump.go
+++ cmd/db/dump.go
@@ -2,19 +2,42 @@ package cmd
 
 import (
 	"os"
+	"log"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 
 	cmdenv "github.com/previousnext/mysql-toolkit/cmd/env"
 	"github.com/previousnext/mysql-toolkit/internal/dumper"
+	"github.com/pkg/errors"
 )
 
 type cmdDump struct {
 	params dumper.DumpParams
+	file string
+	config string
 }
 
 func (cmd *cmdDump) run(c *kingpin.ParseContext) error {
-	return dumper.Dump(os.Stdout, cmd.params)
+	cfg, err := dumper.Load(cmd.config)
+	if err != nil {
+		return errors.Wrap(err, "failed to load config")
+	}
+
+	cmd.params.Config = cfg
+	cmd.params.Logger = log.New(os.Stderr, "", 0)
+	cmd.params.SQLWriter = os.Stdout
+
+	// Use stdout if no output file specified.
+	if cmd.file != "" {
+		cmd.params.Logger.Println("Opening file for writing:", cmd.file)
+		writer, err := os.Create(cmd.file)
+		if err != nil {
+			return err
+		}
+		defer writer.Close()
+	}
+
+	return dumper.Dump(cmd.params)
 }
 
 // Dump declares the "dump" subcommand.
@@ -30,6 +53,6 @@ func Dump(app *kingpin.CmdClause) {
 	cmd.Flag("protocol", "Protocol for connecting to Mysql").Default("tcp").Envar(cmdenv.MySQLProtocol).StringVar(&c.params.Connection.Protocol)
 	cmd.Flag("port", "Port for connecting to Mysql").Default("3306").Envar(cmdenv.MySQLPort).StringVar(&c.params.Connection.Port)
 	cmd.Flag("max-conn", "Maximum amount of open connections").Default("50").Envar(cmdenv.MySQLMaxConn).IntVar(&c.params.Connection.MaxConn)
-	cmd.Flag("config", "Policy for dumping the database").Default("config.yml").Envar(cmdenv.MySQLConfig).StringVar(&c.params.Config)
-	cmd.Flag("file", "Location to save the dumped database").Required().Envar(cmdenv.MySQLFile).StringVar(&c.params.File)
+	cmd.Flag("config", "Policy for dumping the database").Default("config.yml").Envar(cmdenv.MySQLConfig).StringVar(&c.config)
+	cmd.Flag("file", "Location to save the dumped database. Omit to send to stdout.").Envar(cmdenv.MySQLFile).StringVar(&c.file)
 }
