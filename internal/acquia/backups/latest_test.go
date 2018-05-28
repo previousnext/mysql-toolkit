e404bf082dd634924669e01ace064ae1ca7d18a2
Nick Santamaria <nick.santamaria@previousnext.com.au>
2018-05-28 16:23:32 +1000

    Added unit test for findLatest() method.

---
+++ internal/acquia/backups/latest_test.go
@@ -0,0 +1,31 @@
+package backups
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindLatest(t *testing.T) {
+	// The second item in this slice should be returned as the latest.
+	Backups := []Backup{
+		{
+			ID:        1,
+			Completed: 1527488211, // 28 May 2018 06:16:51
+			Link:      "http://foo.bar/baz/1",
+		},
+		{
+			ID:        2,
+			Completed: 1527488212, // 29 May 2018 06:16:51
+			Link:      "http://foo.bar/baz/2",
+		},
+		{
+			ID:        3,
+			Completed: 1524982611, // 29 April 2018 06:16:51
+			Link:      "http://foo.bar/baz/3",
+		},
+	}
+
+	actual := findLatest(Backups)
+	assert.Equal(t, 2, actual.ID, "latest backup returned")
+}
