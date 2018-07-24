67ec7a29df70ca16045297ad3984c4efd31ad99a
Nick Schuch <nick@myschuch.com>
2018-07-24 15:33:42 +1000

    Update container build workflow

---
+++ containers/cli/scripts/database-sanitize.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export MYSQL_HOSTNAME=127.0.0.1
+export MYSQL_USERNAME=tmp
+export MYSQL_PASSWORD=tmp
+export MYSQL_DATABASE=tmp
+
+export AWS_CODEBUILD_DOCKERFILE=/root/Dockerfile
+export AWS_CODEBUILD_SPEC=/root/buildspec.yml
+export MYSQL_CONFIG=/root/config.yml
+
+SOURCE=$1
+TARGET=$2
+
+echo "Starting MySQL"
+mysql-start &
+while ! nc -z localhost 3306; do
+  sleep 0.1
+done
+
+echo "Creating temporary database"
+database-create $MYSQL_DATABASE $SOURCE
+
+echo "Creating temporary account"
+user-create $MYSQL_DATABASE $MYSQL_USERNAME $MYSQL_PASSWORD
+
+echo "Dumping sanitized database"
+mtk db dump > $TARGET
+
+echo "Stopping MySQL"
+mysql-stop
