770eabb56951c43c725257904dd7073d72d43300
Nick Schuch <nick@previousnext.com.au>
2018-09-14 12:39:23 +1000

    Move internal/operator/handler/acquia/job to internal/operator/handler/acquia/generate

---
+++ internal/operator/handler/acquia/generate/const.go
@@ -0,0 +1,22 @@
+package generate
+
+const (
+	// KeyAcquiaUsername for authentication.
+	KeyAcquiaUsername = "acquia.username"
+	// KeyAcquiaPassword for authentication.
+	KeyAcquiaPassword = "acquia.password"
+	// KeyDockerUsername for authentication.
+	KeyDockerUsername = "docker.username"
+	// KeyDockerPassword for authentication.
+	KeyDockerPassword = "docker.password"
+	// KeyAWSRole for AWS CodeBuild role assume.
+	KeyAWSRole = "aws.role"
+	// KeyAWSKey for authentication.
+	KeyAWSKey = "aws.key.id"
+	// KeyAWSAccess for authentication.
+	KeyAWSAccess = "aws.key.access"
+	// KeyAWSBucket for AWS CodeBuild to consume during a built.
+	KeyAWSBucket = "aws.bucket"
+	// KeyMtkConfig file for mtk ruleset.
+	KeyMtkConfig = "mtk.yml"
+)
